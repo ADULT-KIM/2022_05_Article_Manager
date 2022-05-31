37468a107e65d9b0fbac19281049ffc2fde8e098
ADULT-KIM <gongbu0126@gmail.com>
2022-05-31 19:08:08 +0900

    게시물 작성 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -4,19 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==프로그램 시작==");
+		System.out.println("==프로그램 시작==");;
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.printf("명령어 : ");
-		String command;
-		command = sc.next();
-		System.out.printf("입력된 명령어 : %s\n", command);
 		
-		int num = sc.nextInt();
-		System.out.printf("입력된 정수 : %d\n", num);
+		// 몇 번째 글이 작성되었는지 글 번호
+		int lastArticleId = 0;
+		
+		while (true) {
+			System.out.printf("명령어 ) ");
+			String command = sc.nextLine();
+			
+			if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요.");
+				continue;
+			}
+			
+			if (command.equals("system exit")) {
+				break;
+			}
+			if (command.equals("article write")) {
+				int id  = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.println("제목 : \n");
+				String title = sc.nextLine();
+				System.out.println("내용 : \n");
+				String body = sc.nextLine();
+				
+//				System.out.printf("%s, %s\n", title, body);
+				
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+			}
+			
+			else if (command.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			}
+			else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+			}
+		}
 		
 		sc.close();
-
+		
 		System.out.println("==프로그램 끝==");
-			}
+	}
 }
